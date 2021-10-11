1956d284dc444a653432b33f8cb5b1e89134ff63
Abdul Salam K A <32676744+abduzalam@users.noreply.github.com>
2021-10-11 16:00:52 +0530

    Add files via upload

---
+++ Polygon.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Polygon.Library
+{
+    public class ConcreteRegularPolygon
+    {
+        public int NumberOfSides {  get; set; }
+        public int SideLength {  get; set; }
+
+        public ConcreteRegularPolygon(int sides, int lengths)
+        {
+            NumberOfSides = sides;
+            SideLength = lengths;
+        }
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+        //GetArea is declared virtual because the calculation varies based on the Polygon
+        //So its up to the child class to implement the correct formula
+
+        //Even if child class does not implement the GetArea, the compilation will pass,
+        //and during run time GetArea of base class gets executed and we get NotImplementedException
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Polygon.Library/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon.Library
+{
+    public class Square : ConcreteRegularPolygon
+    {
+        //Here number 4 will pass to base class constructor , that is ConcreteRegularPolygon and mapped to Parameter sides
+        public Square(int length):base(4,length)
+        {}
+        //Our own implementation of GetArea by overriding base class GetArea method
+
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+        
+    }
+}
